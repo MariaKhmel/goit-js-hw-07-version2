f7befc65bd618987ac0829f4b5ed488b96366063
MariaKhmel <mariakhmel8@gmail.com>
2023-09-24 00:12:50 +0300

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const refs = {
+    galleryList: document.querySelector('.gallery'),
+
+}
+
+function gallerymarkUp(items) {
+    return items.map(item => {
+        const { preview, original, description } = item;
+        return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+   </a>
+</li>`
+    }).join('');
+    
+}
+
+const markUp = gallerymarkUp(galleryItems);
+refs.galleryList.insertAdjacentHTML('afterbegin', markUp);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
